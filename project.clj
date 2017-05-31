7af1ad878b523f85a48a3044cc12eb5efe2b9f8e
Geir H. Pettersen <geir@guttab.no>
2017-06-01 01:12:23 +0200

    Processing json files to make compressed clojure dumps. Added processed files into resources.

--- project.clj
+++ project.clj
@@ -7,10 +7,12 @@
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [net.mikera/core.matrix "0.60.3"]
                  ;[net.mikera/vectorz-clj "0.47.0"]
-                 [uncomplicate/neanderthal "0.10.0"]]
+                 [uncomplicate/neanderthal "0.10.0"]
+                 [org.clojure/data.json "0.2.6"]]
 
   :main ^:skip-aot machine-learning.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.8.3"]]}}
-  :plugins [[lein-midje "3.2.1"]])
+  :plugins [[lein-midje "3.2.1"]]
+  :jvm-opts ["-Xmx8g"])
---
+++ src/machine_learning/mnist_reader.clj
@@ -0,0 +1,46 @@
+(ns machine-learning.mnist-reader
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [clojure.core.matrix :as m])
+  (:import (java.util.zip GZIPOutputStream)))
+
+
+(defn reshape-inputs [inputs]
+  (mapv #(m/reshape % [784, 1]) inputs))
+
+(defn vectorized-result [digit]
+  (assoc (m/zero-array [10,1]) digit [1.0]))
+
+(defn save! [filename data]
+  (with-open [w (-> filename
+                    io/output-stream
+                    GZIPOutputStream.
+                    io/writer)]
+    (binding [*out* w *print-dup* true] (prn data))))
+
+(defn read-json [filename]
+  (with-open [reader (io/reader (io/file filename) :encoding "UTF-8")]
+    (json/read reader)))
+
+(defn process-resources []
+  (println "Processing mnist_training.json...")
+  (let [training_data (read-json "target/mnist_training.json")
+        training_inputs (reshape-inputs (first training_data))
+        training_results (mapv vectorized-result (second training_data))]
+    (println "  writing training_data.clj.gz...")
+    (save! "target/training_data.clj.gz" (mapv vector training_inputs training_results))
+    (println "  done"))
+
+  (println "Processing mnist_validation.json...")
+  (let [validation_data (read-json "target/mnist_validation.json")
+        validation_inputs (reshape-inputs (first validation_data))]
+    (println "  writing validation_data.clj.gz...")
+    (save! "target/validation_data.clj.gz" (mapv vector validation_inputs (second validation_data)))
+    (println "  done"))
+
+  (println "Processing mnist_testing.json...")
+  (let [testing_data (read-json "target/mnist_testing.json")
+        testing_inputs (reshape-inputs (first testing_data))]
+    (println "  writing testing_data.clj.gz...")
+    (save! "target/testing_data.clj.gz" (mapv vector testing_inputs (second testing_data)))
+    (println "  done")))
