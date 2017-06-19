5f42c7a74482f2cab7ade3c1efcc315b1e3fa390
Geir H. Pettersen <geir@guttab.no>
2017-06-19 13:27:38 +0200

    Implemented dump of png image for reports

---
+++ src/machine_learning/report/image.clj
@@ -0,0 +1,24 @@
+(ns machine-learning.report.image
+  (:require [clojure.java.io :as io])
+  (:import (java.awt.image BufferedImage)
+           (java.awt Color)
+           (javax.imageio ImageIO)
+           (java.io File)))
+
+(defn as-rgb [intensity]
+  (let [gray (Math/round ^Float (* (- 1 intensity) 255.0))]
+    (.getRGB (Color. gray gray gray))))
+
+(defn set-pixel [b_image pos pixel]
+  (.setRGB b_image (quot pos 28) (mod pos 28) (as-rgb pixel)))
+
+(defn image-for [image]
+  (let [bi (BufferedImage. 28 28 BufferedImage/TYPE_INT_RGB)]
+    (dorun (map  #(set-pixel bi %1 (first %2)) (range 0 784) image))
+    bi))
+
+(defn save-image-png [image filename]
+  (let [^BufferedImage bi (image-for image)
+        g (.createGraphics bi)]
+    (ImageIO/write bi "png" ^File (io/as-file filename))))
+
